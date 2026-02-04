c4de13f5c549d0c45e617b75a78fb46f5ae8a24e
Alexandre C Souza Junior <souzajunioralexandre340@gmail.com>
2026-02-04 15:29:08 -0400

    Adicionando pasta correta de onde estão os posts

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -1,12 +1,29 @@
 import { PostModel } from '@/models/post/post-model';
+import { readFile } from 'fs/promises';
+import { resolve } from 'path';
 import { PostRepository } from './post-repository';
 
 const ROOT_DIR = process.cwd();
+const JSON_POSTS_FILE_PATH = resolve(
+   ROOT_DIR,
+   'src',
+   'db',
+   'seed',
+   'posts.json',
+);
 
 export class JsonPostRepository implements PostRepository {
-   private async readFromDisk() {}
+   private async readFromDisk(): Promise<PostModel[]> {
+      const jsonContent = await readFile(JSON_POSTS_FILE_PATH, 'utf-8');
+      const parsedJson = JSON.parse(jsonContent);
+      const { posts } = parsedJson;
+      return posts;
+   }
 
-   async findAll(): Promise<PostModel[]> {}
+   async findAll(): Promise<PostModel[]> {
+      const posts = await this.readFromDisk();
+      return posts;
+   }
 }
 
 export const postRepository = new JsonPostRepository();
