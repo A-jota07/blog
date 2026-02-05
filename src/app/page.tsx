0c200ef4209c191f3260c404bdf381bad36be5f8
Alexandre C Souza Junior <souzajunioralexandre340@gmail.com>
2026-02-05 13:35:58 -0400

    Adicionando Header e criando breakpoints do tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,13 @@
 import { Container } from '@/components/Container';
-import { PostsList } from '@/components/PostsList';
+import { Header } from '@/components/Header';
+// import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/spinLoader';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
    return (
       <Container>
-         <header>
-            <h1 className='text-6xl font-bold text-center py-8'>
-               Aqui é a HEADER
-            </h1>
-         </header>
-         <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
-            nulla provident ipsum, aliquid magnam eum harum facere neque quia
-            totam doloremque vel vero, autem dolorem ut suscipit repudiandae a
-            velit? Ducimus, ex amet quidem cupiditate placeat reiciendis
-            consectetur laboriosam. Ut minima illo perspiciatis cumque nihil hic
-            tenetur quibusdam enim corrupti.
-         </p>
+         <Header />
          <Suspense fallback={<SpinLoader />}>{/* <PostsList /> */}</Suspense>
          <footer>
             <p className='text-6xl font-bold text-center py-8'>Footer</p>
