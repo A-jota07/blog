dce32e10c2eb2a0e443a550efd1702deea73b14c
Alexandre C Souza Junior <souzajunioralexandre340@gmail.com>
2026-02-05 10:55:21 -0400

    Container do site e adicionando estilos Tailwind Css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,28 @@
+import { Container } from '@/components/Container';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/spinLoader';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
    return (
-      <div className='text-slate-900 bg-slate-100 min-h-screen'>
+      <Container>
          <header>
             <h1 className='text-6xl font-bold text-center py-8'>
                Aqui é a HEADER
             </h1>
          </header>
+         <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
+            nulla provident ipsum, aliquid magnam eum harum facere neque quia
+            totam doloremque vel vero, autem dolorem ut suscipit repudiandae a
+            velit? Ducimus, ex amet quidem cupiditate placeat reiciendis
+            consectetur laboriosam. Ut minima illo perspiciatis cumque nihil hic
+            tenetur quibusdam enim corrupti.
+         </p>
          <Suspense fallback={<SpinLoader />}>{/* <PostsList /> */}</Suspense>
          <footer>
             <p className='text-6xl font-bold text-center py-8'>Footer</p>
          </footer>
-      </div>
+      </Container>
    );
 }
