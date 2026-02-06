6c33b192e65429260602240cca2ff9e83c541038
Alexandre C Souza Junior <souzajunioralexandre340@gmail.com>
2026-02-06 09:43:30 -0400

    Removendo propriedade css

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ export function Header() {
       <header>
          <h1
             className={clsx(
-               'text-4xl/normal font-extrabold py-8 transition-all',
+               'text-4xl/normal font-extrabold py-8',
                'sm:text-5xl/normal sm:py-10',
                'md:text-6xl/normal md:py-11',
                'lg:text-7xl/normal lg:py-12',
