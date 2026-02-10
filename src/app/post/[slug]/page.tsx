b88403f6245346d0fee41ed9c65b5877679acacb
Alexandre C Souza Junior <souzajunioralexandre340@gmail.com>
2026-02-10 09:11:22 -0400

    Lançando notFound caso não encontre nenhum post publicado

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,8 +1,22 @@
+import { findBySlugCache } from '@/lib/post/queries';
+import { notFound } from 'next/navigation';
+
 type PostSlugPageProps = {
    params: Promise<{ slug: string }>;
 };
 
 export default async function PostPage({ params }: PostSlugPageProps) {
    const { slug } = await params;
+
+   let post;
+
+   try {
+      post = await findBySlugCache(slug);
+   } catch {
+      post = undefined;
+   }
+
+   if (!post) notFound();
+
    return <h1 className='text-7xl font-extrabold py-16'>PostPage: {slug}</h1>;
 }
--- src/lib/post/queries.ts
+++ src/lib/post/queries.ts
@@ -4,3 +4,11 @@ import { cache } from 'react';
 export const findAllPublicPosts = cache(
    async () => await postRepository.findAllPublic(),
 );
+
+export const findBySlugCache = cache(
+   async (slug: string) => await postRepository.findBySlug(slug),
+);
+
+export const findByIdCache = cache(
+   async (id: string) => await postRepository.findBySlug(id),
+);
