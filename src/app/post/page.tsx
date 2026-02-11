c5ae77536a3936cb2f205f66862753480e32a19c
Alexandre C Souza Junior <souzajunioralexandre340@gmail.com>
2026-02-11 10:27:37 -0400

    Adicionando SSG na page no slug dos posts

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,6 +1,6 @@
 import { SinglePost } from '@/components/SinglePost';
 import { SpinLoader } from '@/components/spinLoader';
-import { findAllPublicPosts, findBySlugCached } from '@/lib/post/queries';
+import { findBySlugCached } from '@/lib/post/queries';
 import { Metadata } from 'next';
 import { Suspense } from 'react';
 
@@ -18,19 +18,6 @@ export async function generateMetadata({
       title: post.title,
       description: post.excerpt,
    };
-}
-
-export async function generateStaticParams() {
-   const posts = await findAllPublicPosts();
-   const params = posts.map(post => {
-      return {
-         slug: post.slug,
-      };
-   });
-
-   console.log(params);
-
-   return params;
 }
 
 export default async function PostPage({ params }: PostSlugPageProps) {
