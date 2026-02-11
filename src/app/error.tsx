824178b67ba7128faf284cf14f2b4326efd2a7ec
Alexandre C Souza Junior <souzajunioralexandre340@gmail.com>
2026-02-11 09:36:04 -0400

    Erro geral da aplicação

---
+++ src/app/error.tsx
@@ -0,0 +1,20 @@
+'use Client';
+
+import ErrorMessage from '@/components/ErrorMessage';
+import { useEffect } from 'react';
+
+type RootErrorPageProps = {
+   error: Error;
+   reset: () => void;
+};
+export default function RootErrorPage({ error }: RootErrorPageProps) {
+   useEffect(() => {}, [error]);
+
+   return (
+      <ErrorMessage
+         pageTitle='Internal Server Error'
+         contentTitle='501'
+         content='Ocorreu um erro do qual nossa aplicação não conseguiu se recuperar. Tente novamente mais tarde'
+      />
+   );
+}
