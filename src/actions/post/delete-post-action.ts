6807589da3dbd1d89c7139d20b4a1f2ac1de8df1
Alexandre C Souza Junior <souzajunioralexandre340@gmail.com>
2026-02-16 10:41:41 -0400

    Usando server actions e useTransition em componentes do cliente

--- src/actions/post/delete-post-action.ts
+++ src/actions/post/delete-post-action.ts
@@ -1,5 +1,7 @@
 'use server';
 
-export async function deletePostAdction(formData: FormData) {
-   const id = formData.get('id');
+import { logColor } from '@/utils/log-color';
+
+export async function deletePostAdction(id: string) {
+   logColor('' + id);
 }
--- src/components/admin/DeletePostButton/index.tsx
+++ src/components/admin/DeletePostButton/index.tsx
@@ -1,20 +1,35 @@
+'use client';
+
 import clsx from 'clsx';
 import { Trash2Icon } from 'lucide-react';
+import { useTransition } from 'react';
 
 type DeletePostButtonProps = {
    id: string;
    title: string;
 };
+
 export function DeletePostButton({ id, title }: DeletePostButtonProps) {
+   const [isPending, startTransition] = useTransition();
+
+   function handleClick() {
+      if (!confirm('tem certeza?')) return;
+
+      startTransition(async () => {});
+   }
+
    return (
       <button
          className={clsx(
             'text-red-500 cursor-pointer transition',
             '[&_svg]:w-4 [&_svg]:h-4 ',
             'hover:scale-130 hover:text-red-700',
+            'disabled:text-slate-300 disabled:cursor-not-allowed',
          )}
          aria-label={`Apagar post: ${id}`}
          title={`Apagar post: ${title}`}
+         onClick={handleClick}
+         disabled={isPending}
       >
          <Trash2Icon />
       </button>
