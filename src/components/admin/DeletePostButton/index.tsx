0fd43ba3ac6a91b59ab74c2fd3e437b6fe3849b8
Alexandre C Souza Junior <souzajunioralexandre340@gmail.com>
2026-02-17 09:49:59 -0400

    Separando o dialogo para um client componente e adicionando função para apagar ou não o post.

--- src/components/admin/DeletePostButton/index.tsx
+++ src/components/admin/DeletePostButton/index.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { deletePostAdction } from '@/actions/post/delete-post-action';
+import { Dialog } from '@/components/Dialog';
 import clsx from 'clsx';
 import { Trash2Icon } from 'lucide-react';
-import { useTransition } from 'react';
+import { useState, useTransition } from 'react';
 
 type DeletePostButtonProps = {
    id: string;
@@ -11,27 +13,46 @@ type DeletePostButtonProps = {
 
 export function DeletePostButton({ id, title }: DeletePostButtonProps) {
    const [isPending, startTransition] = useTransition();
+   const [showDialog, setShowDialog] = useState(false);
 
    function handleClick() {
-      if (!confirm('tem certeza?')) return;
+      setShowDialog(true);
+   }
 
-      startTransition(async () => {});
+   function handleConfirm() {
+      startTransition(async () => {
+         const result = deletePostAdction(id);
+         alert(`post excluido ${result}`);
+         setShowDialog(false);
+      });
    }
 
    return (
-      <button
-         className={clsx(
-            'text-red-500 cursor-pointer transition',
-            '[&_svg]:w-4 [&_svg]:h-4 ',
-            'hover:scale-130 hover:text-red-700',
-            'disabled:text-slate-300 disabled:cursor-not-allowed',
+      <>
+         <button
+            className={clsx(
+               'text-red-500 cursor-pointer transition',
+               '[&_svg]:w-4 [&_svg]:h-4 ',
+               'hover:scale-130 hover:text-red-700',
+               'disabled:text-slate-300 disabled:cursor-not-allowed',
+            )}
+            aria-label={`Apagar post: ${id}`}
+            title={`Apagar post: ${title}`}
+            onClick={handleClick}
+            disabled={isPending}
+         >
+            <Trash2Icon />
+         </button>
+         {showDialog && (
+            <Dialog
+               isVisible={showDialog}
+               title='Apagar Post?'
+               content={`Tem certeza que deseja apagar o post: ${title}`}
+               onCancel={() => setShowDialog(false)}
+               onConfirm={handleConfirm}
+               disabled={isPending}
+            />
          )}
-         aria-label={`Apagar post: ${id}`}
-         title={`Apagar post: ${title}`}
-         onClick={handleClick}
-         disabled={isPending}
-      >
-         <Trash2Icon />
-      </button>
+      </>
    );
 }
