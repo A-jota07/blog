f335415771756ebc30749e3a12acfb58c0ee0512
Alexandre C Souza Junior <souzajunioralexandre340@gmail.com>
2026-02-18 14:08:52 -0400

    Criando o InputCheckbox

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,24 +1,29 @@
 import { Button } from '@/components/Button';
+import { InputCheckbox } from '@/components/InputCheckbox';
 import { InputText } from '@/components/InputText';
-import { TrashIcon } from 'lucide-react';
 
 export const dynamic = 'force-static';
 
 export default async function AdminPostNewPage() {
    return (
-      <div className='flex flex-col gap-6'>
-         <InputText
-            labelText='Nome'
-            placeholder='Digite seu nome'
-            type='name'
-         />
+      <form action='' className='mb-16'>
+         <div className='flex flex-col gap-6'>
+            <InputText
+               labelText='Nome'
+               placeholder='Digite seu nome'
+               type='password'
+            />
+            <InputText
+               labelText='Sobrenome'
+               placeholder='Digite seu sobrenome'
+            />
 
-         <InputText
-            labelText='Senha'
-            placeholder='Digite seu nome'
-            type='password'
-         />
-         <InputText labelText='Sobrenome' placeholder='Digite seu sobrenome' />
-      </div>
+            <InputCheckbox labelText='Sobrenome' />
+
+            <div className='mt-4'>
+               <Button type='submit'>Enviar</Button>
+            </div>
+         </div>
+      </form>
    );
 }
