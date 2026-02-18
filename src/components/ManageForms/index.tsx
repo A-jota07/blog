7b38b233bfbdafafd06b620314957e8b9f31cb34
Alexandre C Souza Junior <souzajunioralexandre340@gmail.com>
2026-02-18 19:49:47 -0400

    Criando o editor Markdon utilizando @uiw/react-md-editor

--- src/components/ManageForms/index.tsx
+++ src/components/ManageForms/index.tsx
@@ -1,10 +1,14 @@
 'use client';
 
+import { useState } from 'react';
 import { Button } from '../Button';
 import { InputCheckbox } from '../InputCheckbox';
 import { InputText } from '../InputText';
+import { MarkdownEditor } from '../MarkdownEditor';
 
 export function ManagePostForm() {
+   const [contentValue, setContentValue] = useState('');
+
    return (
       <form action='' className='mb-16'>
          <div className='flex flex-col gap-6'>
@@ -19,6 +23,14 @@ export function ManagePostForm() {
             />
 
             <InputCheckbox labelText='Sobrenome' />
+
+            <MarkdownEditor
+               labelText='Conteúdo'
+               disabled={false}
+               textAreaName='content'
+               value={contentValue}
+               setValue={setContentValue}
+            />
 
             <div className='mt-4'>
                <Button type='submit'>Enviar</Button>
