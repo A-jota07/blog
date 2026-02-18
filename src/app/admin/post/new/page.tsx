d0753bf7217437f513bafa293bf4080334098bda
Alexandre C Souza Junior <souzajunioralexandre340@gmail.com>
2026-02-18 10:51:43 -0400

    Criando o componente de InputText para a area admin quando for criar algum post.

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,5 +1,24 @@
+import { Button } from '@/components/Button';
+import { InputText } from '@/components/InputText';
+import { TrashIcon } from 'lucide-react';
+
 export const dynamic = 'force-static';
 
 export default async function AdminPostNewPage() {
-   return <div className='py-16 text-6xl'>AdminPostNewPage</div>;
+   return (
+      <div className='flex flex-col gap-6'>
+         <InputText
+            labelText='Nome'
+            placeholder='Digite seu nome'
+            type='name'
+         />
+
+         <InputText
+            labelText='Senha'
+            placeholder='Digite seu nome'
+            type='password'
+         />
+         <InputText labelText='Sobrenome' placeholder='Digite seu sobrenome' />
+      </div>
+   );
 }
