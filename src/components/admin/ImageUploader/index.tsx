640c6517c291dbe26d77b2c71df7013edd6ae730
Alexandre C Souza Junior <souzajunioralexandre340@gmail.com>
2026-02-19 13:19:25 -0400

    Validando o tamanho de arquivo que pode ser enviado no ImageUploader

--- src/components/admin/ImageUploader/index.tsx
+++ src/components/admin/ImageUploader/index.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import { Button } from '@/components/Button';
+import { IMAGE_UPLOAD_MAX_SIZE } from '@/lib/constants';
 import clsx from 'clsx';
 import { ImageUpIcon } from 'lucide-react';
 import { useRef } from 'react';
+import { toast } from 'react-toastify';
 
 export function ImageUploader() {
    const fileInputRef = useRef<HTMLInputElement>(null);
@@ -12,6 +14,28 @@ export function ImageUploader() {
       if (!fileInputRef.current) return;
 
       fileInputRef.current.click();
+   }
+
+   function handleChange() {
+      if (!fileInputRef.current) return;
+
+      const fileInput = fileInputRef.current;
+      const file = fileInput?.files?.[0];
+
+      if (!file) return;
+
+      if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
+         const readableMaxSize = IMAGE_UPLOAD_MAX_SIZE / 1024;
+         toast.error(`Imagem muito grande. Máx.: ${readableMaxSize}KB.`);
+
+         fileInput.value = '';
+         return;
+      }
+
+      const formData = new FormData();
+      formData.append('file', file);
+
+      fileInput.value = '';
    }
 
    const ImageClasses = clsx(
@@ -34,6 +58,7 @@ export function ImageUploader() {
          </Button>
 
          <input
+            onChange={handleChange}
             ref={fileInputRef}
             className='hidden'
             name='file'
