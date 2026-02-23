8937e03226ea54cc8c56fa58b9910e74414a091c
Alexandre C Souza Junior <souzajunioralexandre340@gmail.com>
2026-02-23 14:26:18 -0400

    campos para posts no formulario

--- src/components/admin/ManageForms/index.tsx
+++ src/components/admin/ManageForms/index.tsx
@@ -14,26 +14,69 @@ export function ManagePostForm() {
       <form action='' className='mb-16'>
          <div className='flex flex-col gap-6'>
             <InputText
-               labelText='Nome'
-               placeholder='Digite seu nome'
-               type='password'
+               labelText='ID'
+               name='id'
+               placeholder='ID gerado automaticamente'
+               type='text'
+               defaultValue={''}
+               readOnly
             />
+
             <InputText
-               labelText='Sobrenome'
-               placeholder='Digite seu sobrenome'
+               labelText='Slug'
+               name='slug'
+               placeholder='Slug gerado automaticamente'
+               type='text'
+               defaultValue={''}
+               readOnly
+            />
+
+            <InputText
+               labelText='Título'
+               name='title'
+               placeholder='Digite o título'
+               type='text'
+               defaultValue={''}
+            />
+
+            <InputText
+               labelText='Autor'
+               name='author'
+               placeholder='Digite o nome do autor'
+               type='text'
+               defaultValue={''}
+            />
+
+            <InputText
+               labelText='Excerto'
+               name='Excerpt'
+               placeholder='Digite o resumo'
+               type='text'
+               defaultValue={''}
+            />
+
+            <MarkdownEditor
+               labelText='Conteúdo'
+               value={contentValue}
+               setValue={setContentValue}
+               textAreaName='content'
             />
 
             <ImageUploader />
 
-            <MarkdownEditor
-               labelText='Conteúdo'
-               disabled={false}
-               textAreaName='content'
-               value={contentValue}
-               setValue={setContentValue}
+            <InputText
+               labelText='URL da imagem de capa'
+               name='coverImageUrl'
+               placeholder='Digite a url da imagem'
+               type='text'
+               defaultValue={''}
             />
 
-            <InputCheckbox labelText='Sobrenome' />
+            <InputCheckbox
+               labelText='Publicar?'
+               name='published'
+               type='checkbox'
+            />
 
             <div className='mt-4'>
                <Button type='submit'>Enviar</Button>
