91ca94d81fc7c2d055386100a93660f72ccee593
Alexandre C Souza Junior <souzajunioralexandre340@gmail.com>
2026-02-24 09:36:06 -0400

    Preenchendo dados dos posts com Data Transfer Object

--- src/app/admin/post/[id]/page.tsx
+++ src/app/admin/post/[id]/page.tsx
@@ -1,4 +1,14 @@
+import { ManagePostForm } from '@/components/admin/ManageForms';
+import { makePublicPost } from '@/dto/post/dto';
+import { findPostByIdAdmin } from '@/lib/post/queries/admin';
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+
 export const dynamic = 'force-dynamic';
+
+export const metadata: Metadata = {
+   title: 'Editar Post',
+};
 
 type AdminPostIdPageProps = {
    params: Promise<{
@@ -10,5 +20,16 @@ export default async function AdminPostIdPage({
    params,
 }: AdminPostIdPageProps) {
    const { id } = await params;
-   return <div className='py-16 text-6xl'>Page Post id admin {id}</div>;
+   const post = await findPostByIdAdmin(id).catch();
+
+   if (!post) notFound();
+
+   const publicPost = makePublicPost(post);
+
+   return (
+      <div className='flex flex-col gap-6'>
+         <h1 className='text-xl font-extrabold'>Editar Post</h1>
+         <ManagePostForm publicPost={publicPost} />
+      </div>
+   );
 }
